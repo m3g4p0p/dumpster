45b386db659439297e6ac0af4049e72654d88e83
m3g4p0p <bassistsagainstracists@gmx.net>
2016-08-25 08:05:55 +0200

    Create dumpster.js

---
+++ dumpster.js
@@ -0,0 +1,71 @@
+(window === undefined ? global : window).createStore = function(...reducers) {
+
+  const states = [];
+  const listeners = [];
+  const history = [];
+
+  /**
+   * Call the listeners with the current state
+   */
+  const _notify = function(self) {
+    const state = states[states.length - 1];
+    
+    listeners.forEach(listener => {
+      listener.call(self, state);
+    });   
+  };
+
+  return {
+
+    /**
+     * Subscribe a listener function
+     */
+    subscribe(listener) {
+      listeners.push(listener);
+    },
+
+    /**
+     * Dispatch an action, which passes the current
+     * state from reducer to reducer, pushes the resulting
+     * state and notifies all subscribers
+     */
+    dispatch(action) {
+      const state = reducers.reduce(
+        (carry, reducer) => reducer(carry, action), 
+        states[states.length - 1]
+      );
+      
+      states.push(state);
+      _notify(this);
+    },
+
+    /**
+     * Get the current state
+     */
+    get() {
+      return states[states.length - 1];
+    },
+
+    /**
+     * Undo the last action
+     */
+    undo() {
+
+      if (!states.length) return;
+
+      history.push(states.pop());
+      _notify(this);
+    },
+
+    /**
+     * Redo the last undone action
+     */
+    redo() {
+
+      if (!history.length) return;
+
+      states.push(history.pop());
+      _notify(this);
+    }
+  };
+};
